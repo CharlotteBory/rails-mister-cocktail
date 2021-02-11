c73ba7093b3cbb4f0d46496faeeb7145b1e1e39d
Charlotte Bory <cmmbory@gmail.com>
2021-02-11 18:10:37 +0100

    back ok

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'json'
+require 'uri'
+
+url = "https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list"
+
+ingredients = JSON.parse(open(url).read)
+p "Adding ingredients..."
+
+ingredients["drinks"].each do |ingredient|
+  Ingredient.create!(name: ingredient["strIngredient1"])
+end
+
+p "#{ingredients.first.count} ingredients added!"
